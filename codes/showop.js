3724679d38e841d5aae28b8763e8afb2ffd29d30
HypeerZ <matheusrmds@gmail.com>
2023-05-12 13:56:52 -0300

    calculator-uptade

---
+++ codes/showop.js
@@ -0,0 +1,8 @@
+function showOp(){
+    var show = document.querySelector("article.show")
+    var h3 = document.querySelector("h3.h3")
+
+    show.style.display = "block"
+    h3.style.fontSize = "xx-large"
+    h3.style.padding = "15px"
+}
